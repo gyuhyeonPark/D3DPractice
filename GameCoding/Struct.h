4200901004590fa8ec55ecb6472e81860a05bbdb
park <park@gmail.com>
2024-11-04 09:14:09 +0900

    루키스 D3D 첫 커밋

---
+++ GameCoding/Struct.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Types.h"
+
+struct Vertex
+{
+	Vec3 postion;
+	Color color;
+};
---
+++ GameCoding/pch.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Types.h"
+#include "Values.h"
+#include "Struct.h"
+
+// STL
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+// WIN
+#include <windows.h>
+#include <assert.h>
+
+// DX
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+/// 프로젝트 설정 -> 추가 포함 디렉터리로 경로를 설정, 해당 경로의 헤더 및 인라인 파일을 체크해서 가능한 것.
+#include <DirectXTex/DirectXTex.h>
+#include <DirectXTex/DirectXTex.inl>
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#ifdef _DEBUG
+#pragma comment(lib, "DirectXTex\\DirectXTex_debug.lib")
+#else
+#pragma comment(lib, "DirectXTex\\DirectXTex.lib")
+#endif
+
+#define CHECK(p)	assert(SUCCEEDED(p))
