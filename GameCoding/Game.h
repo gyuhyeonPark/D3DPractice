e5f0b896f9ccae739c096c17d08d7a8abe602a35
gyuhyeonPark <gmfrwh12@gmail.com>
2025-01-13 18:08:15 +0900

    Transform에서 계층구조를 형성해 테스트 완료.

--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Graphics.h"
+#include "GameObject.h"
+
 class Game
 {
 public:
@@ -14,36 +17,9 @@ public:
 
 private:
 	HWND _hwnd;
-	//uint32 _width = 0;
-	//uint32 _height = 0;
-
+	
 	shared_ptr<Graphics> _graphics;
 	shared_ptr<Pipeline> _pipeline;
-
-private:
-	// Geometry
-	shared_ptr<Geometry<VertexTextureData>> _geometry;
-	shared_ptr<VertexBuffer> _vertexBuffer;
-	shared_ptr<IndexBuffer> _indexBuffer;
-	shared_ptr<InputLayout> _inputLayout;
-
-	shared_ptr<VertexShader> _vertexShader;
-	shared_ptr<PixelShader> _pixelShader;
-	
-	shared_ptr<Texture> _texture1;
-	shared_ptr<Texture> _texture2;
-
-	shared_ptr<RasterizerState> _rasterizerState;
-	shared_ptr<SamplerState> _samplerState;
-	shared_ptr<BlendState> _blendState;
-
-private:
-	// SRT
-	TransformData _transformData;
-	shared_ptr<ConstantBuffer<TransformData>> _constantBuffer;
-
-	Vec3 _localPosition = { 0.f, 0.f, 0.f };
-	Vec3 _localRotation = { 0.f, 0.f, 0.f };
-	Vec3 _localScale = { 1.f, 1.f, 1.f };
+	shared_ptr<GameObject> _gameObject;
 };
 
